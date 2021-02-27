484b67078f8d012a6c10d7be4e9486b609ef3b25
Matvii Hodovaniuk <matvii@hodovani.uk>
2021-02-27 20:57:54 +0200

    Init tree preview

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,18 @@ if (process.env.NODE_ENV === 'development') {
 	require('../index.html');
 }
 
+const treeView = document.getElementById('treeView');
+
+fetch(
+	'https://github.com/fancycode/pylzma/raw/master/tests/data/test-issue-43.7z'
+)
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		console.log(data);
+	});
+
 document
 	.getElementById('file')
 	.addEventListener('change', async (e) => {
@@ -17,12 +29,26 @@ document
 		const archive = await Archive.open(file);
 		let obj = await archive.extractFiles();
 		console.log(obj);
+		walk(obj);
 
-		const reader = new FileReader();
-		reader.onload = function (event) {
-			const textarea = document.getElementById('fileOutput');
-			textarea.textContent += event.target.result;
-			console.log(event.target.result);
-		};
-		reader.readAsText(obj['blah.txt']);
+		// const reader = new FileReader();
+		// reader.onload = function (event) {
+		// 	const textarea = document.getElementById('fileOutput');
+		// 	textarea.textContent += event.target.result;
+		// 	console.log(event.target.result);
+		// };
+		// reader.readAsText(obj['blah.txt']);
 	});
+
+function walk(node) {
+	if (node instanceof File) {
+		console.log(node);
+		const li = document.createElement('li');
+		li.innerText = node.name;
+		treeView.appendChild(li);
+	} else {
+		Object.keys(node).forEach((key) => {
+			walk(node[key]);
+		});
+	}
+}
