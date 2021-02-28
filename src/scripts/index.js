1aa9c4ff70322a0dabe20df8199a7022659c5a6d
Matvii Hodovaniuk <matvii@hodovani.uk>
2021-02-28 13:05:32 +0200

    Add static tree view

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import '../styles/index.scss';
 import { Archive } from 'libarchive.js/main.js';
+import { v4 as uuidv4 } from 'uuid';
 
 Archive.init({
 	workerUrl: 'public/worker-bundle.js',
@@ -9,17 +10,7 @@ if (process.env.NODE_ENV === 'development') {
 	require('../index.html');
 }
 
-const treeView = document.getElementById('treeView');
-
-fetch(
-	'https://github.com/fancycode/pylzma/raw/master/tests/data/test-issue-43.7z'
-)
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
-	});
+const textarea = document.getElementById('fileOutput');
 
 document
 	.getElementById('file')
@@ -28,27 +19,40 @@ document
 
 		const archive = await Archive.open(file);
 		let obj = await archive.extractFiles();
-		console.log(obj);
-		walk(obj);
 
-		// const reader = new FileReader();
-		// reader.onload = function (event) {
-		// 	const textarea = document.getElementById('fileOutput');
-		// 	textarea.textContent += event.target.result;
-		// 	console.log(event.target.result);
-		// };
-		// reader.readAsText(obj['blah.txt']);
+		walk({ node: obj, liId: 'treeView', name: file.name });
 	});
 
-function walk(node) {
-	if (node instanceof File) {
+function walk({ node, liId, name }) {
+	const root = document.getElementById(liId);
+	if (!(node instanceof File)) {
 		console.log(node);
+		const newUlId = uuidv4();
+		const newUl = document.createElement('ul');
+		newUl.id = newUlId;
+
+		const newLi = document.createElement('li');
+		root.appendChild(newLi);
+
+		const span = document.createElement('span');
+		span.innerText = name;
+		newLi.appendChild(span);
+
+		root.appendChild(newUl);
+
+		Object.keys(node).forEach((key) => {
+			walk({ node: node[key], liId: newUlId, name: key });
+		});
+	} else {
 		const li = document.createElement('li');
 		li.innerText = node.name;
-		treeView.appendChild(li);
-	} else {
-		Object.keys(node).forEach((key) => {
-			walk(node[key]);
+		li.addEventListener('click', () => {
+			const reader = new FileReader();
+			reader.onload = function (event) {
+				textarea.textContent += event.target.result;
+			};
+			reader.readAsText(node);
 		});
+		root.appendChild(li);
 	}
 }
